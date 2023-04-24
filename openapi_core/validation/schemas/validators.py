48ef16a5e3fa605418e7f3c87b54924fcad24cc0
p1c2u <maciag.artur@gmail.com>
2023-04-24 12:14:43 +0100

    multi types schema format unmarshal fix

--- openapi_core/validation/schemas/validators.py
+++ openapi_core/validation/schemas/validators.py
@@ -78,6 +78,22 @@ class SchemaValidator:
 
         return lambda x: True
 
+    def get_primitive_type(self, value: Any) -> str:
+        schema_types = self.schema.getkey("type")
+        if isinstance(schema_types, str):
+            return schema_types
+        if schema_types is None:
+            schema_types = sorted(self.validator.TYPE_CHECKER._type_checkers)
+        assert isinstance(schema_types, list)
+        for schema_type in schema_types:
+            result = self.type_validator(value, type_override=schema_type)
+            if not result:
+                continue
+            result = self.format_validator(value)
+            if not result:
+                continue
+            return schema_type
+
     def iter_valid_schemas(self, value: Any) -> Iterator[Spec]:
         yield self.schema
 
@@ -143,3 +159,13 @@ class SchemaValidator:
                 log.warning("invalid allOf schema found")
             else:
                 yield subschema
+
+    def iter_valid_schemas(self, value: Any) -> Iterator[Spec]:
+        yield self.schema
+
+        one_of_schema = self.schema_validator.get_one_of_schema(value)
+        if one_of_schema is not None:
+            yield one_of_schema
+
+        yield from self.schema_validator.iter_any_of_schemas(value)
+        yield from self.schema_validator.iter_all_of_schemas(value)
--- tests/integration/unmarshalling/test_unmarshallers.py
+++ tests/integration/unmarshalling/test_unmarshallers.py
@@ -2057,6 +2057,27 @@ class TestOAS31SchemaUnmarshallersFactory(
         assert len(exc_info.value.schema_errors) == 1
         assert "is not of type" in exc_info.value.schema_errors[0].message
 
+    @pytest.mark.parametrize(
+        "types,format,value,expected",
+        [
+            (["string", "null"], "date", None, None),
+            (["string", "null"], "date", "2018-12-13", date(2018, 12, 13)),
+        ],
+    )
+    def test_multiple_types_format_valid_or_ignored(
+        self, unmarshallers_factory, types, format, value, expected
+    ):
+        schema = {
+            "type": types,
+            "format": format,
+        }
+        spec = Spec.from_dict(schema, validator=None)
+        unmarshaller = unmarshallers_factory.create(spec)
+
+        result = unmarshaller.unmarshal(value)
+
+        assert result == expected
+
     def test_any_null(self, unmarshallers_factory):
         schema = {}
         spec = Spec.from_dict(schema, validator=None)
